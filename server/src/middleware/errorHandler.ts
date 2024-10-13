7b48345e7981e9835deeda97cad320a3dfbe508b
VasaR <vasanthias52@gmail.com>
2024-10-13 17:17:31 +0530

    feat: error handler middleware added

---
+++ server/src/middleware/errorHandler.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import { CustomError, responseType } from "../types/types";
+
+const errorHandler = (
+  err: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error(err);
+
+  const isProduction = process.env.IS_PRODUCTION || "true";
+
+  const response: responseType = {
+    success: false,
+    message: err.message || "An unexpected error occurred",
+  };
+
+  if (!isProduction) {
+    response.error = err.stack || err.message || "Error details not available";
+  }
+
+  const statusCode = err.status || 500;
+
+  res.status(statusCode).json(response);
+};
+
+export default errorHandler;
